5746ed2215429aa75bca8b9be6afc5b42e8b42fd
Kigamekun <reksa.prayoga1012@gmail.com>
2021-08-13 20:52:22 +0700

    dahlah males

--- resources/views/privatechat.blade.php
+++ resources/views/privatechat.blade.php
@@ -454,7 +454,15 @@ $('#action_menu_btn').click(function(){
 
 
 <script>
-
+// 	var input = document.getElementById("msg");
+// input.addEventListener("keyup", function(event) {
+//   // Number 13 is the "Enter" key on the keyboard
+//   if (event.keyCode === 13) {
+//     // Cancel the default action, if needed
+//     event.preventDefault();
+//     // Trigger the button element with a click
+//     document.getElementById("send").click();
+//   }
 var id = @json($id);
 
 
